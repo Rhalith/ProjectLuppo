ba41c4bae6de45e52250bc65f1b3793431f04ece
JystXen <oglumoguz@hotmail.com>
2023-07-17 19:05:52 +0300

    Another forgotten comment add-on

--- Assets/Scripts/Managers/UIManager/MainMenuManager.cs
+++ Assets/Scripts/Managers/UIManager/MainMenuManager.cs
@@ -7,16 +7,19 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private GameObject mainMenuUI;
     [SerializeField] private GameObject optionsUI;
 
+    //Starts New Game
     public void StartNewGame()
     {
         ScenesManager.instance.LoadNewGame();
     }
 
+    //Opens Options Menu
     public void OpenOptions()
     {
         optionsUI.SetActive(true);
     }
 
+    //Closes Game
     public void ExitGame() 
     {
         Application.Quit();
