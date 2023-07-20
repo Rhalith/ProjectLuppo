ae3a419735966449807757f6671e407f6c5b15a4
Rhalith <akmannuhyigit@gmail.com>
2023-07-20 14:53:23 +0300

    added sushi controller
    needs to improve

--- Assets/Scripts/Sushi/Sushi.cs
+++ Assets/Scripts/Sushi/Sushi.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class Sushi : MonoBehaviour
+public class Sushi
 {
-    // Start is called before the first frame update
-    void Start()
+    List<string> ingredients;
+
+    public Sushi(params string[] ingredients)
     {
-        
+        foreach (var item in ingredients)
+        {
+            this.ingredients.Add(item);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool CheckIngredients(List<string> availableIngredients)
     {
-        
+        if(ingredients.Count == 0)
+        {
+            Debug.Log("Sushi is empty");
+            return false;
+        }
+        else if (availableIngredients.All(item => ingredients.Contains(item)))
+        {
+            return true;
+        }
+        return false;
     }
 }
--- Assets/Scripts/Sushi/SushiManager.cs
+++ Assets/Scripts/Sushi/SushiManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SushiManager : MonoBehaviour
 {
     private static SushiManager _instance;
-    [SerializeField] private GameObject[] _sushiPrefabs;
-    [SerializeField] private Sushi[] _availableSushis;
+    private List<string> _availableIngredients = new();
+    private List<Sushi> _availabeSushis = new();
+
+    public List<string> AvailableIngredients { get => _availableIngredients; set => _availableIngredients = value; }
+
 
     private void Awake()
     {
@@ -31,5 +35,23 @@ public class SushiManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    List<Sushi> CreateSushis()
+    {
+        List<Sushi> sushiList = new List<Sushi>();
+
+        Sushi sake = new Sushi("somon"); 
+        sushiList.Add(sake);
+        Sushi ebi = new Sushi("karides"); 
+        sushiList.Add(ebi);
+        Sushi ahi = new Sushi("tonbaligi", "soyasosu"); 
+        sushiList.Add(ahi);
+        Sushi unagi = new Sushi("yilanbaligi", "unagisos"); 
+        sushiList.Add(unagi); 
+        Sushi hamachi = new Sushi("sarikuyruk", "soyasosu"); 
+        sushiList.Add(hamachi);
+
+        return sushiList;
     }
 }
