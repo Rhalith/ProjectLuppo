7255c2fd106c3bb0f7aecfc2a3251752d396b31e
JystXen <oglumoguz@hotmail.com>
2023-08-04 11:00:50 +0300

    change: scene changes

--- Assets/CustomerManager.cs
+++ Assets/CustomerManager.cs
@@ -15,29 +15,75 @@ public class CustomerManager : MonoBehaviour
     Quaternion _rotation;
     private int _score;
     private string[] _wantedMaterials;
+    private int orderNumber;
+    WantedFoods wantedSushi;
+
+    enum WantedFoods
+    {
+        SalmonHosomaki,
+        CucumberHosomaki,
+        SalmonCucumberChumaki,
+    }
 
     private void Start()
     {
         //_spawnPoint = GetComponent<GameObject>();
         //_turnPoint = GetComponent<GameObject>();
         //_orderPoint = GetComponent<GameObject>();
-        _position = _spawnPoint.transform.position;
-        _rotation.eulerAngles = new Vector3(0, 90f, 0);
+        _position = _orderPoint.transform.position;
+        _rotation.eulerAngles = new Vector3(0, 0, 0);
+        
+        wantedSushi = WantedFoods.SalmonHosomaki;
 
-        CustomerInstantiate();
+        
 
+    }
+
+    WantedFoods GiveOrder()
+    {
+        if (orderNumber == 0)
+        {
+            wantedSushi = WantedFoods.CucumberHosomaki;
+        }
+        else if (orderNumber == 1)
+        {
+            wantedSushi = WantedFoods.SalmonHosomaki;
+        }
+
+        else if (orderNumber == 2)
+        {
+            wantedSushi = WantedFoods.SalmonCucumberChumaki;
+        }
+
+        return wantedSushi;
     }
 
     private void CustomerInstantiate()
     {
+        orderNumber = Random.Range(0, 3);
         _currentCustomer = _customerPrefabs[Random.Range(0, 3)];
         Instantiate(_currentCustomer, _position, _rotation);
     }
     #endregion
 
+    #region SushiMechanic
+
+
+
+    #endregion
+
+
     #region CustomerMovement
 
     //TODO: movement system. can instantly instantiate for tomorrow.
+
+    IEnumerator SpawnNewCustomer()
+    {
+        CustomerInstantiate();
+        yield return new WaitForSeconds(1.5f);
+        GiveOrder();
+
+    }
 
     #endregion
 }
