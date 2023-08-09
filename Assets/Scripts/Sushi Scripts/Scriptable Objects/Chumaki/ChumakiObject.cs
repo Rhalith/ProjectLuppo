1e35646a613146c9b5d3878e192e15bddeb2e244
merabalarefem <92051290+merabalarefem@users.noreply.github.com>
2023-08-09 15:17:50 +0300

    unnecessary scripts deleted

--- Assets/Scripts/Sushi Scripts/Scriptable Objects/Chumaki/ChumakiObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Chumaki", menuName = "Edible/Sushis/Chumaki")]
-public class ChumakiObject : ScriptableObject
-{
-    public Material[] filling;
-}
--- Assets/Scripts/Sushi Scripts/Scriptable Objects/Futomaki/FutomakiObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Futomaki", menuName = "Edible/Sushis/Futomaki")]
-public class FutomakiObject : ScriptableObject
-{
-    public Material[] filling;
-}
