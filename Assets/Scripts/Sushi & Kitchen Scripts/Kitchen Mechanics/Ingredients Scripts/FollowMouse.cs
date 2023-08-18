82e3ffe4fcf95aa07803c89f412782f09357a965
Hydofbl <hidayetfurkanbirel@gmail.com>
2023-08-18 17:06:13 +0300

    Changes merged with InGame/PlayTest

--- Assets/Scripts/Sushi & Kitchen Scripts/Kitchen Mechanics/Ingredients Scripts/FollowMouse.cs
+++ Assets/Scripts/Sushi & Kitchen Scripts/Kitchen Mechanics/Ingredients Scripts/FollowMouse.cs
@@ -10,6 +10,7 @@ public class FollowMouse : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, 50000.0f))
         {
+            Debug.Log(hit.point);
             transform.position = hit.point;
         }
     }
